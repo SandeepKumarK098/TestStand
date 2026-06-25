8c76b3cb833f1a2494ffb31c1d8e77e21e8a0ff4
SandeepKumarK098 <sandeepsandeep96512@gmail.com>
2026-06-25 14:27:20 +0530

    Commit 1

---
+++ WebPages/TestCases/auto_commit_script.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "REPO_DIR=C:\Users\spkumar\Desktop\TestStand"
+set "COUNTER_FILE=%REPO_DIR%\commit_counter.txt"
+
+cd /d "%REPO_DIR%"
+
+REM Create the counter file starting at 0 if it doesn't exist yet
+if not exist "%COUNTER_FILE%" (
+    echo 0> "%COUNTER_FILE%"
+)
+
+REM Read the current counter value
+set /p num=<"%COUNTER_FILE%"
+
+REM Increment it
+set /a num=num+1
+
+REM Save the new value back for next run
+echo %num%> "%COUNTER_FILE%"
+
+REM Run the git commands
+git add .
+git commit -m "Commit %num%"
+git push -u origin master
+
+echo.
+echo Done. Pushed as "Commit %num%".
+pause
